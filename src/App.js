357159d65bd83e607b82a50446dd1662d25b65ff
tuanva <vuanhtuan743@gmail.com>
2022-03-24 10:01:59 +0700

    update func component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,193 @@
+// import TodoList from "./components/TodoList";
+// import AddTodos from "./components/AddTodos";
+// import React from "react";
+// import { v4 as uuidv4 } from "uuid";
+
+// const filterItem = (items = [], status = "") => {
+//   switch (status) {
+//     case "all":
+//       return items;
+//     case "active":
+//       return items.filter((todo) => !todo.completed);
+//     case "complete":
+//       return items.filter((todo) => todo.completed);
+//     default:
+//       return items;
+//   }
+// };
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       todos: [],
+//       todoFilter: [],
+//       trigger: false,
+//       todoToShow: "all"
+//     };
+//     this.filterTodoCompleted = this.filterTodoCompleted.bind(this);
+//     this.filterClear = this.filterClear.bind(this);
+//     this.filterTodoNotCompleted = this.filterTodoNotCompleted.bind(this);
+//     //this.handleUpdate = this.handleUpdate(this);
+//   }
+//   handleTick = async (kaka) => {
+//     const uuid = uuidv4();
+//     const newApp = this.state.todos.map((todo) => {
+//       if (todo === kaka)
+//         return {
+//           text: todo.text,
+//           completed: !todo.completed,
+//           uuid: uuid,
+//         };
+//       else return todo;
+//     });
+//     await this.setState({ todos: newApp });
+//     console.log(newApp);
+//     localStorage.setItem("todos", JSON.stringify(this.state.todos));
+//   };
+//   selects = async () => {
+//     const uuid = uuidv4();
+//     const newApp = this.state.todos.map((todo) => {
+//       if (this.state.trigger === false)
+//         return { text: todo.text, completed: true, uuid: uuid };
+//       else if (this.state.trigger === true)
+//         return { text: todo.text, completed: false, uuid: uuid };
+//       else return todo;
+//     });
+//     this.setState({ trigger: !this.state.trigger });
+//     await this.setState({ todos: newApp });
+//     localStorage.setItem("todos", JSON.stringify(this.state.todos));
+//   };
+//   handleUpdate = async (todo, tada) => {
+//     const uuid = uuidv4();
+//     const newFix = this.state.todos.map((_todo) => {
+//       if (_todo.uuid === tada.uuid)
+//         return { completed: _todo.completed, text: todo, uuid: uuid };
+//       else return _todo;
+//     });
+//     await this.setState({ todos: newFix });
+//     localStorage.setItem("todos", JSON.stringify(this.state.todos));
+//   };
+//   handleDelete = async (hahaha) => {
+
+//     const newFix = this.state.todos.filter((todo) => todo !== hahaha);
+
+//     await this.setState({ todos: newFix });
+//     localStorage.setItem("todos", JSON.stringify(this.state.todos));
+//   };
+
+//   filterClear = () => {
+//     this.setState({ todoFilter: [] });
+//   };
+//   filterTodoCompleted = async () => {
+//     const todoCompleted = this.state.todos.map((todo) => {
+//       if (todo.completed === true) return todo;
+//       else return "";
+//     });
+//     await this.setState({ todoFilter: todoCompleted });
+//     console.log(this.state.todoFilter);
+//   };
+//   filterTodoNotCompleted = async () => {
+//     const todoNotCompleted = this.state.todos.map((todo) => {
+//       if (todo.completed === false) return todo;
+//       else return "";
+//     });
+//     await this.setState({ todoFilter: todoNotCompleted });
+//     console.log(this.state.todoFilter);
+//   };
+
+//   updateTodoToShow = (s) => {
+//     this.setState({
+//       todoToShow: s,
+//     });
+//   };
+
+//   render() {
+//     let todos = filterItem(this.state.todos, this.state.todoToShow);
+
+//     return (
+//       <div>
+//         <AddTodos addTodoFn={this.addTodo}></AddTodos>
+//         <TodoList
+//           updateTodoFn={this.updateTodo}
+//           handleUpdate={this.handleUpdate}
+//           handleDelete={this.handleDelete}
+//           selects={this.selects}
+//           handleTick={this.handleTick}
+//           handleDeleteAll={this.handleDeleteAll}
+//           trigger={this.state.trigger}
+//           todos={todos}
+//         />
+
+//         <div className="wrap">
+//           <div>
+//             Todos left:{" "}
+//             {this.state.todos.filter((todo) => !todo.completed).length}
+//           </div>
+//           <div>
+//             <button onClick={() => this.updateTodoToShow("all")}>All</button>
+//             <button onClick={() => this.updateTodoToShow("active")}>
+//               Active
+//             </button>
+//             <button onClick={() => this.updateTodoToShow("complete")}>
+//               Completed
+//             </button>
+//             {this.state.todos.some((todo) => todo.complete) ? (
+//               <button onClick={this.removeAllTodosThatAreComplete}>
+//                 {" "}
+//                 Remove all
+//               </button>
+//             ) : null}
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+
+//   componentDidMount = () => {
+//     const todos = localStorage.getItem("todos");
+//     if (todos) {
+//       const savedTodos = JSON.parse(todos);
+//       this.setState({ todos: savedTodos });
+//     } else {
+//       console.log("No todos");
+//     }
+//   };
+//   addTodo = async (todo) => {
+//     const uuid = uuidv4();
+//     await this.setState({
+//       todos: [
+//         ...this.state.todos,
+//         {
+//           text: todo,
+//           completed: false,
+//           uuid: uuid,
+//         },
+//       ],
+//     });
+//     localStorage.setItem("todos", JSON.stringify(this.state.todos));
+//   };
+//   updateTodo = async (todo) => {
+//     const uuid = uuidv4();
+//     const newTodos = this.state.todos.map((_todo) => {
+//       if (todo === _todo)
+//         return {
+//           completed: true,
+//           text: todo.text,
+//           uuid: uuid,
+//         };
+//       else return _todo;
+//     });
+//     await this.setState({ todos: newTodos });
+//     localStorage.setItem("todos", JSON.stringify(this.state.todos));
+//   };
+// }
+// export default App;
+
 import TodoList from "./components/TodoList";
 import AddTodos from "./components/AddTodos";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-
 
 const filterItem = (items = [], status = "") => {
   switch (status) {
@@ -17,23 +202,15 @@ const filterItem = (items = [], status = "") => {
   }
 };
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [],
-      todoFilter: [],
-      trigger: false,
-      todoToShow: "all"
-    };
-    this.filterTodoCompleted = this.filterTodoCompleted.bind(this);
-    this.filterClear = this.filterClear.bind(this);
-    this.filterTodoNotCompleted = this.filterTodoNotCompleted.bind(this);
-    //this.handleUpdate = this.handleUpdate(this);
-  }
-  handleTick = async (kaka) => {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todoFilter, setTodoFilter] = useState([]);
+  const [trigger, setTrigger] = useState([]);
+  const [todoToShow, setTodoToshow] = useState([]);
+
+  const handleTick = (kaka) => {
     const uuid = uuidv4();
-    const newApp = this.state.todos.map((todo) => {
+    const newApp = todos.map((todo) => {
       if (todo === kaka)
         return {
           text: todo.text,
@@ -42,136 +219,78 @@ class App extends React.Component {
         };
       else return todo;
     });
-    await this.setState({ todos: newApp });
-    console.log(newApp);
-    localStorage.setItem("todos", JSON.stringify(this.state.todos));
+    setTodos(newApp);
+    localStorage.setItem("todos", JSON.stringify(todos));
   };
-  selects = async () => {
+
+  const selects = () => {
     const uuid = uuidv4();
-    const newApp = this.state.todos.map((todo) => {
-      if (this.state.trigger === false)
+    const newApp = todos.map((todo) => {
+      if (trigger === false)
         return { text: todo.text, completed: true, uuid: uuid };
-      else if (this.state.trigger === true)
+      else if (trigger === true)
         return { text: todo.text, completed: false, uuid: uuid };
       else return todo;
     });
-    this.setState({ trigger: !this.state.trigger });
-    await this.setState({ todos: newApp });
-    localStorage.setItem("todos", JSON.stringify(this.state.todos));
+    setTrigger(!trigger);
+    setTodos(newApp);
+    localStorage.setItem("todos", JSON.stringify(todos));
   };
-  handleUpdate = async (todo, tada) => {
+  const handleUpdate = async (todo, tada) => {
     const uuid = uuidv4();
-    const newFix = this.state.todos.map((_todo) => {
+    const newFix = todos.map((_todo) => {
       if (_todo.uuid === tada.uuid)
         return { completed: _todo.completed, text: todo, uuid: uuid };
       else return _todo;
     });
-    await this.setState({ todos: newFix });
-    localStorage.setItem("todos", JSON.stringify(this.state.todos));
+    setTodos(newFix);
+    localStorage.setItem("todos", JSON.stringify(todos));
   };
-  handleDelete = async (hahaha) => {
-    
-    const newFix = this.state.todos.filter((todo) => todo !== hahaha);
-    
-    await this.setState({ todos: newFix });
-    localStorage.setItem("todos", JSON.stringify(this.state.todos));
+  const handleDelete = async (hahaha) => {
+    const newFix = todos.filter((todo) => todo !== hahaha);
+    setTodos(newFix);
+    localStorage.setItem("todos", JSON.stringify(todos));
   };
-  
-  filterClear = () => {
-    this.setState({ todoFilter: [] });
+
+  const filterClear = () => {
+    setTodoFilter([]);
   };
-  filterTodoCompleted = async () => {
-    const todoCompleted = this.state.todos.map((todo) => {
+  const filterTodoCompleted = async () => {
+    const todoCompleted = todos.map((todo) => {
       if (todo.completed === true) return todo;
       else return "";
     });
-    await this.setState({ todoFilter: todoCompleted });
-    console.log(this.state.todoFilter);
+    setTodoFilter(todoCompleted);
   };
-  filterTodoNotCompleted = async () => {
-    const todoNotCompleted = this.state.todos.map((todo) => {
+  const filterTodoNotCompleted = async () => {
+    const todoNotCompleted = todos.map((todo) => {
       if (todo.completed === false) return todo;
       else return "";
     });
-    await this.setState({ todoFilter: todoNotCompleted });
-    console.log(this.state.todoFilter);
+    setTodoFilter(todoNotCompleted);
   };
 
-  updateTodoToShow = (s) => {
-    this.setState({
-      todoToShow: s,
-    });
+  const updateTodoToShow = (s) => {
+    setTodoToshow(s);
   };
-  
 
-  render() {
-    let todos = filterItem(this.state.todos, this.state.todoToShow);
-
-    return (
-      <div>
-        <AddTodos addTodoFn={this.addTodo}></AddTodos>
-        <TodoList
-          updateTodoFn={this.updateTodo}
-          handleUpdate={this.handleUpdate}
-          handleDelete={this.handleDelete}
-          selects={this.selects}
-          handleTick={this.handleTick}
-          handleDeleteAll={this.handleDeleteAll}
-          trigger={this.state.trigger}
-          todos={todos}
-        />
-
-        <div className="wrap">
-          <div>
-            Todos left:{" "}
-            {this.state.todos.filter((todo) => !todo.completed).length}
-          </div>
-          <div>
-            <button onClick={() => this.updateTodoToShow("all")}>All</button>
-            <button onClick={() => this.updateTodoToShow("active")}>
-              Active
-            </button>
-            <button onClick={() => this.updateTodoToShow("complete")}>
-              Completed
-            </button>
-            {this.state.todos.some((todo) => todo.complete) ? (
-              <button onClick={this.removeAllTodosThatAreComplete}>
-                {" "}
-                Remove all
-              </button>
-            ) : null}
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  componentDidMount = () => {
-    const todos = localStorage.getItem("todos");
-    if (todos) {
-      const savedTodos = JSON.parse(todos);
-      this.setState({ todos: savedTodos });
-    } else {
-      console.log("No todos");
-    }
-  };
-  addTodo = async (todo) => {
+  const addTodo = (todo) => {
     const uuid = uuidv4();
-    await this.setState({
-      todos: [
-        ...this.state.todos,
-        {
-          text: todo,
-          completed: false,
-          uuid: uuid,
-        },
-      ],
-    });
-    localStorage.setItem("todos", JSON.stringify(this.state.todos));
+
+    setTodos([
+      ...todos,
+      {
+        text: todo,
+        completed: false,
+        uuid: uuid,
+      },
+    ]);
+    localStorage.setItem("todos", JSON.stringify(todos));
   };
-  updateTodo = async (todo) => {
+
+  const updateTodo = async (todo) => {
     const uuid = uuidv4();
-    const newTodos = this.state.todos.map((_todo) => {
+    const newTodos = todos.map((_todo) => {
       if (todo === _todo)
         return {
           completed: true,
@@ -180,8 +299,50 @@ class App extends React.Component {
         };
       else return _todo;
     });
-    await this.setState({ todos: newTodos });
-    localStorage.setItem("todos", JSON.stringify(this.state.todos));
+    setTodos(newTodos);
+    localStorage.setItem("todos", JSON.stringify(todos));
   };
-}
+  useEffect(() => {
+    const todos = localStorage.getItem("todos");
+    if (todos) {
+      const savedTodos = JSON.parse(todos);
+      setTodos(savedTodos);
+    } else {
+      console.log("No todos");
+    }
+  }, []);
+
+  let _todos = filterItem(todos, todoToShow);
+  return (
+    <div>
+      <AddTodos addTodoFn={addTodo}></AddTodos>
+      <TodoList
+        updateTodoFn={updateTodo}
+        handleUpdate={handleUpdate}
+        handleDelete={handleDelete}
+        selects={selects}
+        handleTick={handleTick}
+        // handleDeleteAll={handleDeleteAll}
+        trigger={trigger}
+        todos={_todos}
+      />
+
+      <div className="wrap">
+        <div>Todos left: {todos.filter((todo) => !todo.completed).length}</div>
+        <div>
+          <button onClick={() => updateTodoToShow("all")}>All</button>
+          <button onClick={() => updateTodoToShow("active")}>Active</button>
+          <button onClick={() => updateTodoToShow("complete")}>
+            Completed
+          </button>
+          {todos.some((todo) => todo.complete) ? (
+            // <button onClick={removeAllTodosThatAreComplete}> Remove all</button>
+            <button> Remove all</button>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default App;
--- src/components/FilterTodo.js
+++ src/components/FilterTodo.js
@@ -1,65 +1,126 @@
+// import React, { Component } from "react";
+// import { Fragment } from "react/cjs/react.production.min";
+// import TodoItem from "./TodoItem";
+
+// export default class FilterTodo extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { checked: true, completed: true };
+//     this.todosRef = [];
+//     this.selects = this.selects.bind(this);
+//     //this.handleUpdate = this.handleUpdate.bind(this);
+//     this.handleDeleteAll = this.handleDeleteAll.bind(this);
+//   }
+//   selects() {
+//     this.props.selects();
+//   }
+//   updateTodo = (todo) => {
+//     this.props.updateTodoFn(todo);
+//   };
+//   handleUpdate = (todo, tada) => {
+//     this.props.handleUpdate(todo, tada);
+//   };
+//   handleDelete = (hahaha) => {
+//     this.props.handleDelete(hahaha);
+//   };
+//   handleTick = (kaka) => {
+//     this.props.handleTick(kaka);
+//   };
+//   handleDeleteAll = () => {
+//     this.props.handleDeleteAll();
+//   };
+//   render() {
+//     const { todoFilter } = this.props;
+//     return (
+//       <div className="todoListContainer">
+//         <div className="checkbox-wrapper">
+//           <input
+//             type="checkbox"
+//             id="abc"
+//             onChange={this.selects}
+//             checked={this.props.trigger ? true : false}
+//           />
+//           <label htmlFor="abc"></label>
+//         </div>
+//         {todoFilter.map((_todoFilter, _index) =>
+//           _todoFilter === "" ? (
+//             <Fragment key={_index} />
+//           ) : _todoFilter.completed === true ? (
+//             <div key={_index}>
+//               <label htmlFor={_index}></label>
+//               <TodoItem
+//                 updateTodoFn={this.updateTodo}
+//                 handleUpdate={this.handleUpdate}
+//                 handleDelete={this.handleDelete}
+//                 handleTick={this.handleTick}
+//                 key={_index}
+//                 todo={_todoFilter}
+//               ></TodoItem>
+//             </div>
+//           ) : (
+//             <Fragment key={_index} />
+//           )
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
 import React, { Component } from "react";
 import { Fragment } from "react/cjs/react.production.min";
 import TodoItem from "./TodoItem";
 
-export default class FilterTodo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { checked: true, completed: true };
-    this.todosRef = [];
-    this.selects = this.selects.bind(this);
-    //this.handleUpdate = this.handleUpdate.bind(this);
-    this.handleDeleteAll = this.handleDeleteAll.bind(this);
-  }
-  selects() {
-    this.props.selects();
-  }
-  updateTodo = (todo) => {
-    this.props.updateTodoFn(todo);
+const FilterTodo = (props) => {
+  const selects = () => {
+    props.selects();
   };
-  handleUpdate = (todo, tada) => {
-    this.props.handleUpdate(todo, tada);
+  const updateTodo = (todo) => {
+    props.updateTodoFn(todo);
   };
-  handleDelete = (hahaha) => {
-    this.props.handleDelete(hahaha);
+  const handleUpdate = (todo, tada) => {
+    props.handleUpdate(todo, tada);
   };
-  handleTick = (kaka) => {
-    this.props.handleTick(kaka);
+  const handleDelete = (hahaha) => {
+    props.handleDelete(hahaha);
   };
-  handleDeleteAll = () => {
-    this.props.handleDeleteAll();
+  const handleTick = (kaka) => {
+    props.handleTick(kaka);
   };
-  render() {
-    const { todoFilter } = this.props;
-    return (
-      <div className="todoListContainer">
-        <div className="checkbox-wrapper">
+  const handleDeleteAll = () => {
+    props.handleDeleteAll();
+  };
+  return (
+    <div className="todoListContainer">
+      <div className="checkbox-wrapper">
         <input
           type="checkbox"
           id="abc"
-          onChange={this.selects}
-          checked={this.props.trigger ? true : false}
-        /><label htmlFor="abc"></label></div>
-        {todoFilter.map((_todoFilter, _index) =>
-          _todoFilter === "" ? (
-            <Fragment key={_index} />
-          ) : _todoFilter.completed === true ? (
-            <div key={_index}>
-              <label htmlFor={_index}></label>
-              <TodoItem
-                updateTodoFn={this.updateTodo}
-                handleUpdate={this.handleUpdate}
-                handleDelete={this.handleDelete}
-                handleTick={this.handleTick}
-                key={_index}
-                todo={_todoFilter}
-              ></TodoItem>
-            </div>
-          ) : (
-            <Fragment key={_index} />
-          )
-        )}
+          onChange={selects}
+          checked={props.trigger ? true : false}
+        />
+        <label htmlFor="abc"></label>
       </div>
-    );
-  }
-}
+      {props.todoFilter.map((_todoFilter, _index) =>
+        _todoFilter === "" ? (
+          <Fragment key={_index} />
+        ) : _todoFilter.completed === true ? (
+          <div key={_index}>
+            <label htmlFor={_index}></label>
+            <TodoItem
+              updateTodoFn={updateTodo}
+              handleUpdate={handleUpdate}
+              handleDelete={handleDelete}
+              handleTick={handleTick}
+              key={_index}
+              todo={_todoFilter}
+            ></TodoItem>
+          </div>
+        ) : (
+          <Fragment key={_index} />
+        )
+      )}
+    </div>
+  );
+};
+
+export default FilterTodo;
